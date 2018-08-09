e5f2c4c2b4b789c80e7d61b6d49f6f5eb7eeaf1a
docoding <developer70@indglobal-consulting.com>
2018-08-09 16:14:14 +0530

    Ball moving within a boundary

--- ball.js
+++ ball.js
@@ -1,5 +1,6 @@
 var canvas = document.getElementById('can');
 var context = canvas.getContext('2d');
+var raf;
 
 canvas.width = window.innerWidth;
 canvas.height = (window.innerHeight/4)*3;
@@ -7,6 +8,8 @@ canvas.height = (window.innerHeight/4)*3;
 var ball = {
     x: 50,
     y: 200,
+    vx: 5,
+    vy: 5,
     radius: 30,
     color: '#E91E63',
     draw: function(){
@@ -19,4 +22,23 @@ var ball = {
     }
 }
 
-ball.draw();
+var update = function(){
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    
+    ball.x += ball.vx;
+    ball.y += ball.vy;
+    if(ball.x + ball.vx + ball.radius > canvas.width || ball.x + ball.vx - ball.radius < 0)
+        ball.vx = -ball.vx;
+    if(ball.y + ball.vy + ball.radius > canvas.height || ball.y + ball.vy - ball.radius < 0)
+        ball.vy = -ball.vy;
+    ball.draw()
+    raf = window.requestAnimationFrame(update);
+}
+
+canvas.addEventListener('mouseover', function(e){
+    raf = window.requestAnimationFrame(update);
+});
+
+canvas.addEventListener('mouseout', function(e){
+    window.cancelAnimationFrame(raf);
+});
